ba9e481bf026a610bb6ecf8942ce121f071642e6
Philippe Damen <mr.deleeuw@gmail.com>
2016-04-26 15:30:01 +0200

    Authcontroller revoke token.

--- src/main/java/be/ordina/ordineo/auth/controller/AuthController.java
+++ src/main/java/be/ordina/ordineo/auth/controller/AuthController.java
@@ -1,15 +1,27 @@
 package be.ordina.ordineo.auth.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.exceptions.InvalidClientException;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
 @RestController
 public class AuthController {
 
+    @Autowired
+    private DefaultTokenServices defaultTokenServices;
+
     @RequestMapping("/user")
     public Principal user(Principal user) {
         return user;
+    }
+
+    @RequestMapping(value = "/oauth/token/revoke", method = RequestMethod.POST)
+    public @ResponseBody
+    void revoke(@RequestParam("token") String value) throws InvalidClientException {
+        defaultTokenServices.revokeToken(value);
+        //TODO remove from redis
     }
 }
