b9957757af9ae3c889abc86513f642a4657a4adf
kevindahub <kevin.van.houtte1@gmail.com>
2016-04-26 17:45:34 +0200

    add EnableResourceServer

--- src/main/java/be/ordina/ordineo/auth/AuthApplication.java
+++ src/main/java/be/ordina/ordineo/auth/AuthApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
+@EnableResourceServer
 @EnableAuthorizationServer
 public class AuthApplication  extends WebMvcConfigurerAdapter {
 
